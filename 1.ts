de0faf70527cb3639aa65e000467e8c83db25a34
laopo001 <353272497@qq.com>
2021-12-30 16:16:10 +0800

    fix

--- 1.ts
+++ 1.ts
@@ -5,3 +5,26 @@ function run() {
 }
 
 run(1)
+
+
+
+function res() {
+    try {
+        a.b = 1;
+        return 1;
+    } catch (e) {
+        return 2;
+    }
+}
+console.log(res()); // 2 
+function res() {
+    try {
+        a.b = 1;
+        return 1;
+    } catch (e) {
+        return 2;
+    } finally {
+        return 3;
+    }
+}
+console.log(res()); // 3
